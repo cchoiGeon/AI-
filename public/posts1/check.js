37107c971e91435cb29eb20f2d11732ba0954362
choigeon <qrwrqrwrer@naver.com>
2025-02-12 18:18:51 +0900

    fix: posts/1 에 js 수정완료

--- public/posts1/check.js
+++ public/posts1/check.js
@@ -7,34 +7,39 @@ document.addEventListener('DOMContentLoaded', async () => {
     contentElement.style.display = 'none';
 
     try {
-        const logincheckResponse = await fetch(`https://asia-northeast3-life-legacy-dev.cloudfunctions.net/api/auth/status`, {
-            method: 'GET',
-            credentials: 'include',
-        });
-        const loginCheckResult = await logincheckResponse.json(); // 중복 방지 위해 변수명 수정
+        // const logincheckResponse = await fetch(`https://asia-northeast3-life-legacy-dev.cloudfunctions.net/api/auth/status`, {
+        //     method: 'GET',
+        //     credentials: 'include',
+        // });
+        // const loginCheckResult = await logincheckResponse.json(); // 중복 방지 위해 변수명 수정
 
-        if (loginCheckResult.code === "200") {
-            const apiResponse = await fetch(`https://asia-northeast3-life-legacy-dev.cloudfunctions.net/api/posts/check/1`, {
-                method: 'GET',
-                credentials: 'include',
-            });
+        // if (loginCheckResult.code === "200") {
+        //     const apiResponse = await fetch(`https://asia-northeast3-life-legacy-dev.cloudfunctions.net/api/posts/check/1`, {
+        //         method: 'GET',
+        //         credentials: 'include',
+        //     });
     
-            const apiResult = await apiResponse.json(); // 변수명 중복 방지
-            console.log(apiResult);
+        //     const apiResult = await apiResponse.json(); // 변수명 중복 방지
+        //     console.log(apiResult);
 
-            if (apiResult.code == "200") {
-                window.location.href = "/posts/2";
-            } else {
-                loadingElement.style.display = 'none';
-                contentElement.style.display = 'flex';
+        //     if (apiResult.code == "200") {
+        //         window.location.href = "/posts/2";
+        //     } else {
+        //         loadingElement.style.display = 'none';
+        //         contentElement.style.display = 'flex';
                 
-                const checkJsCompleteEvent = new Event('checkJsComplete');
-                document.dispatchEvent(checkJsCompleteEvent);
-            }
-        } else {
-            alert("로그인을 해주세요!");
-            window.location.href = "/login";
-        }
+        //         const checkJsCompleteEvent = new Event('checkJsComplete');
+        //         document.dispatchEvent(checkJsCompleteEvent);
+        //     }
+        // } else {
+        //     alert("로그인을 해주세요!");
+        //     window.location.href = "/login";
+        // }
+        loadingElement.style.display = 'none';
+        contentElement.style.display = 'flex';
+        
+        const checkJsCompleteEvent = new Event('checkJsComplete');
+        document.dispatchEvent(checkJsCompleteEvent);
     } catch (error) {
         console.error('Error fetching profile:', error);
     }
